8b3bdba23965ee499b8b41d04cb5e4716325cee6
Parker Rogers <parker@paradime.io>
2024-04-17 10:42:22 +0000

    added new models

---
+++ models/sources/stg_tmdb_movie_2.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+    SELECT 
+TMDB_ID,
+        TITLE,
+        ORIGINAL_TITLE,
+        OVERVIEW,
+        TAGLINE,
+        KEYWORDS,
+        RELEASE_DATE,
+        RUNTIME,
+        BUDGET,
+        REVENUE,
+        VOTE_AVERAGE,
+        IMDB_ID,
+        GENRE_NAMES,
+    FROM 
+        {{ source('PARADIME_MOVIE_CHALLENGE', 'TMDB_MOVIES') }}
+)
+
+SELECT 
+    * 
+FROM 
+    source
+
