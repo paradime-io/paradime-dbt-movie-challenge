bc8ca1df1453d49b910c8cd8df384109bd59d31e
Parker Rogers <parker@paradime.io>
2024-04-17 09:04:05 +0000

    creating project

---
+++ models/sources/stg_omdb_movies.sql
@@ -0,0 +1,35 @@
+WITH source AS (
+    SELECT 
+        IMDB_ID,
+        TITLE,
+        DIRECTOR,
+        WRITER,
+        ACTORS,
+        GENRE,
+        LANGUAGE,
+        COUNTRY,
+        TYPE,
+        PLOT,
+        RATED,
+        RATINGS,
+        METASCORE,
+        IMDB_RATING,
+        IMDB_VOTES,
+        RELEASED_DATE,
+        RELEASE_YEAR,
+        RUNTIME,
+        DVD,
+        BOX_OFFICE,
+        POSTER,
+        PRODUCTION,
+        WEBSITE,
+        AWARDS,
+        TMDB_ID
+    FROM 
+        {{ source('PARADIME_MOVIE_CHALLENGE', 'OMDB_MOVIES') }}
+)
+
+SELECT 
+    * 
+FROM 
+    source
