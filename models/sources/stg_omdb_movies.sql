638ca360520cd9e00aa088cc8b104fea65f4f099
Spence Perry <sperry@classy.org>
2024-04-28 20:32:01 +0000

    add RT score to omdb staging model

--- models/sources/stg_omdb_movies.sql
+++ models/sources/stg_omdb_movies.sql
@@ -28,8 +28,20 @@ WITH source AS (
     FROM 
         {{ source('PARADIME_MOVIE_CHALLENGE', 'OMDB_MOVIES') }}
 )
-
+, rt_scores as (
+    select
+        imdb_id, 
+        rats.value:Source, 
+        -- get rotten tomatoes score from json object
+        max(substring(rats.value:Value, 0, length(rats.value:Value)-1)::int) as rotten_tomatoes_score
+    from source mov, lateral flatten (input => mov.ratings) rats
+    where rats.value:Source = 'Rotten Tomatoes'
+    group by 1, 2
+)
 SELECT 
-    * 
+    source.*,
+    rt_scores.rotten_tomatoes_score
 FROM 
     source
+left join rt_scores
+    on source.imdb_id = rt_scores.imdb_id
