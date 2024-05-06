f327160707cb1c2525eee0bc9a49ac74694ae02f
Isin Pesch <isin.pesch@deel.com>
2024-05-06 13:14:18 +0000

    sqlfluff config

--- models/intermediate/base/int_movies_mapping.sql
+++ models/intermediate/base/int_movies_mapping.sql
@@ -1,53 +1,53 @@
 with final as (
     select
-        -- IDs
+    -- IDs
         tmdb.tmdb_id,
         tmdb.imdb_id,
 
         -- Strings
-        coalesce(omdb.title, tmdb.title) as title,
         tmdb.original_title,
         tmdb.overview,
-        --tmdb.tagline,
         tmdb.keywords,
-        coalesce(omdb.genre, tmdb.genre_names) as genre,
-        coalesce(omdb.language, tmdb.original_language) as language,
+        --tmdb.tagline,
         omdb.director,
         omdb.writer,
         omdb.actors,
+        omdb.type,
         tmdb.status,
         tmdb.homepage,
+        tmdb.vote_average,
+        tmdb.vote_count,
+        tmdb.revenue,
         --tmdb.poster_path,
         --tmdb_backdrop_path,
         --tmdb.spoken_languages,
         --tmdb.production_company_names,
         --tmdb.production_country_names,
-        
+
         -- Numbers
-        coalesce(omdb.runtime, tmdb.runtime) as runtime,
-        tmdb.vote_average,
-        tmdb.vote_count,
-        tmdb.revenue,
         tmdb.budget,
         omdb.imdb_rating,
         omdb.imdb_votes,
         omdb.box_office,
         omdb.awards,
+        tmdb.video as is_video,
+        omdb.release_year,
+        coalesce(omdb.title, tmdb.title) as title,
+        coalesce(omdb.genre, tmdb.genre_names) as genre,
 
         -- Booleans
-        tmdb.video as is_video,
+        coalesce(omdb.language, tmdb.original_language) as language,
 
         -- Dates/Timestamps
-       coalesce(omdb.released_date, tmdb.release_date) as release_date,
-       omdb.release_year,
+        coalesce(omdb.runtime, tmdb.runtime) as runtime,
+        coalesce(omdb.released_date, tmdb.release_date) as release_date
 
     from
-        {{ ref('stg_tmdb_movies') }} tmdb
-    left join {{ ref('stg_omdb_movies') }} omdb
-        on tmdb.imdb_id = omdb.imdb_id
+        {{ ref('stg_tmdb_movies') }} as tmdb
+        left join {{ ref('stg_omdb_movies') }} as omdb
+            on tmdb.imdb_id = omdb.imdb_id
 )
 
-select
-    *
+select *
 from
     final
--- models/intermediate/int_collaboration_network_actor_director.sql
+++ models/intermediate/int_collaboration_network_actor_director.sql
@@ -1,32 +1,43 @@
-with collaborations as(
-SELECT
-    {{ dbt_utils.generate_surrogate_key(['d.value', 'a.value']) }} as director_actor_key,
-    TRIM(d.value) AS DIRECTOR,
-    TRIM(a.value) AS ACTOR,  -- Cleaning up any extra spaces around actor names
-    IMDB_ID,
-    imdb_rating
-FROM
-    {{ ref('int_movies_mapping') }},
-    LATERAL FLATTEN(input => SPLIT(DIRECTOR, ',')) AS d,
-    LATERAL FLATTEN(input => SPLIT(ACTORS, ',')) AS a  -- Splitting and flattening the actor list
-WHERE
-    DIRECTOR IS NOT NULL AND ACTORS IS NOT NULL AND IMDB_ID IS NOT NULL  -- Ensuring non-null values for processing
-GROUP BY
-    TRIM(d.value), TRIM(a.value), IMDB_ID, imdb_rating, director_actor_key
+with collaborations as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['d.value', 'a.value']) }} as director_actor_key,
+        trim(d.value) as director,
+        trim(a.value) as actor,  -- Cleaning up any extra spaces around actor names
+        m.imdb_id,
+        m.title,
+        m.imdb_rating
+    from
+        {{ ref('int_movies_mapping') }} as m,
+        lateral flatten(input => split(director, ',')) as d,
+        lateral flatten(input => split(actors, ',')) as a  -- Splitting and flattening the actor list
+    where
+        m.director is not null and m.director != 'N/A'
+        and m.actors is not null and m.actors != 'N/A'
+        and m.imdb_rating between 0 and 10
+        and m.imdb_id is not null
+    group by
+        1, 2, 3, 4, 5, 6
 ),
 
-avg_ratings as(
+avg_ratings as (
     select
-    director_actor_key,
-    director,
-    actor,
-    avg(imdb_rating) as avg_imdb_rating,
-    --count(distinct imdb_id) as movie_occurence_count
+        director_actor_key,
+        director,
+        actor,
+        avg(imdb_rating) as avg_imdb_rating,
+        count(distinct imdb_id) as movie_occurence_count
     from collaborations
-    group by 1,2,3
-    order by 4 desc
-)
+    group by 1, 2, 3
+    order by 5 desc, 4 desc
+),
 
+movies as (
+    select c.*
+    from avg_ratings as a
+        left join collaborations as c
+            on a.director_actor_key = c.director_actor_key
+    where a.director_actor_key = '3346fd7be1df7e13d4748d539ea96b9e'
+)
 
 
 select * from avg_ratings
