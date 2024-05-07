ef74ad4d5af7484b82885c71ef48e9dbd876698e
Andrew Nelson <andy.nelson@evolvconsulting.com>
2024-05-07 20:45:39 +0000

    k

---
+++ models/marts/analysis/hot_streak_writer_profit.sql
@@ -0,0 +1,11 @@
+with cte1 as (
+select writer, film_cnt_writer
+from {{ ref('mart_writers')}}
+where film_cnt_writer > 2
+qualify row_number() over (order by threefilm_avg_profit desc, writer) = 1
+)
+select *
+from {{ ref('mart_writers')}}
+where writer in (select writer from cte1)
+    and film_cnt_writer between (select film_cnt_writer - 2 from cte1) and (select film_cnt_writer from cte1)
+order by released_date
