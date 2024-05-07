ba89c15bad5482a10f867a2e6bf82942cc75d009
Andrew Nelson <andy.nelson@evolvconsulting.com>
2024-05-07 21:35:19 +0000

    lightdash deploy

--- models/marts/analysis/director_imdb_cold.sql
+++ models/marts/analysis/director_imdb_cold.sql
@@ -2,7 +2,7 @@ with cte1 as (
 select director, film_cnt_director
 from {{ ref('mart_directors')}}
 where film_cnt_director > 2
-qualify row_number() over (order by threefilm_avg_profit asc, director) = 1
+qualify row_number() over (order by threefilm_avg_imdb_rating asc, director) = 1
 )
 select *
 from {{ ref('mart_directors')}}
