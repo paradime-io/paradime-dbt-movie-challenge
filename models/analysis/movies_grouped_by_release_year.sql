ca846aca29c8ff2a8ac2bd8958415fd67e25fa6e
Darko Compagnoni <darko.compagnoni@nimbusintelligence.com>
2024-05-08 13:17:05 +0000

    added movies grouped by release year

---
+++ models/analysis/movies_grouped_by_release_year.sql
@@ -0,0 +1,13 @@
+SELECT
+    RELEASE_YEAR,
+    COUNT(IMDB_ID) as movie_count,
+    AVG(IMDB_RATING) AS rating_avg,
+    SUM(wins) AS wins_count,
+    SUM(nominations) AS nominations_count,
+    SUM(wins)/SUM(nominations)*100 AS wins_nominations_ratio_percent
+FROM   
+    {{ ref('join_tmbd_ombd_genre') }}
+WHERE NOMINATIONS > WINS
+GROUP BY 
+    RELEASE_YEAR
+
