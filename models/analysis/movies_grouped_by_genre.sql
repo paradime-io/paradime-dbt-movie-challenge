22125947f186abf3cc971857f1bdf074804abe0f
Darko Compagnoni <darko.compagnoni@nimbusintelligence.com>
2024-05-08 13:03:52 +0000

    corrected wins_nominations_ratio to visualize it as a percentage on Looker

--- models/analysis/movies_grouped_by_genre.sql
+++ models/analysis/movies_grouped_by_genre.sql
@@ -4,7 +4,7 @@ SELECT
     AVG(IMDB_RATING) AS rating_avg,
     SUM(wins) AS wins_count,
     SUM(nominations) AS nominations_count,
-    SUM(wins)/SUM(nominations) AS wins_nominations_ratio
+    SUM(wins)/SUM(nominations)*100 AS wins_nominations_ratio_percent
 FROM   
     {{ ref('join_tmbd_ombd_genre') }}
 WHERE NOMINATIONS > WINS
