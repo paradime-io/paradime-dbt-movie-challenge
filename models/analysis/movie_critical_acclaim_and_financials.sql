c7532583e269532dcc5a1582f3c7faccb905f49d
Darko Compagnoni <darko.compagnoni@nimbusintelligence.com>
2024-05-08 09:56:26 +0000

    added release year

--- models/analysis/movie_critical_acclaim_and_financials.sql
+++ models/analysis/movie_critical_acclaim_and_financials.sql
@@ -4,6 +4,7 @@ WITH movies_data AS (
         O.TITLE AS TITLE,
         O.DIRECTOR AS DIRECTOR,
         O.WRITER AS WRITER,
+        O.RELEASE_YEAR AS RELEASE_YEAR,
         O.IMDB_VOTES AS IMDB_VOTES,
         O.IMDB_RATING AS IMDB_RATING,
         {{ parse_awards('o.awards') }}
@@ -33,6 +34,7 @@ movies_data_with_financials AS (
         md.TITLE AS TITLE,
         md.DIRECTOR AS DIRECTOR,
         md.WRITER AS WRITER,
+        md.RELEASE_YEAR AS RELEASE_YEAR,
         md.IMDB_VOTES AS IMDB_VOTES,
         md.IMDB_RATING AS IMDB_RATING,
         md.WINS AS WINS,
