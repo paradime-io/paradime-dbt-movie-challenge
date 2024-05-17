378544a10fdd9ad71d7c2b2f023b65489c2d87c6
Blosher Brar <blosher13@gmail.com>
2024-05-17 12:45:28 +0000

    fix issue with percentages

--- models/dmarts/dim_movies_genre.sql
+++ models/dmarts/dim_movies_genre.sql
@@ -1,14 +1,5 @@
-WITH m AS (
-    SELECT * FROM {{ ref('inter_movies_genre_pivot') }}
-)
-,
-nmp as (
-    select * from {{ref('inter_num_movies_per_year')}}
+WITH inter_movies_genre_per_year_percentage AS (
+    SELECT * FROM {{ ref('inter_movies_genre_per_year_percentage') }}
 )
 
-select
-    m.*,
-    nmp.total_movies
-from m
-left join nmp
-    on m.release_year = nmp.release_year
+select * from inter_movies_genre_per_year_percentage
