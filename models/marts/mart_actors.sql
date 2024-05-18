dc8e68484105991462643a9f178de81be67f2e07
Andrew Nelson <andy.nelson@evolvconsulting.com>
2024-05-18 14:33:47 +0000

    udpate

--- models/marts/mart_actors.sql
+++ models/marts/mart_actors.sql
@@ -8,9 +8,9 @@ select u.actor
      , m.runtime
      , m.metascore 
      , m.imdb_rating
-     , avg(m.profit)      over (partition by u.actor order by m.release_date rows between 2 preceding and current row)   as threefilm_avg_profit
-     , avg(m.imdb_rating) over (partition by u.actor order by m.release_date rows between 2 preceding and current row)   as threefilm_avg_imdb_rating
-     , avg(m.metascore)   over (partition by u.actor order by m.release_date rows between 2 preceding and current row)   as threefilm_avg_meta_score
+     , to_number(avg(m.profit)      over (partition by u.actor order by m.release_date rows between 2 preceding and current row))   as threefilm_avg_profit
+     , to_number(avg(m.imdb_rating) over (partition by u.actor order by m.release_date rows between 2 preceding and current row),10,1)   as threefilm_avg_imdb_rating
+     , to_number(avg(m.metascore)   over (partition by u.actor order by m.release_date rows between 2 preceding and current row),10,1)   as threefilm_avg_meta_score
      , row_number()       over (partition by u.actor order by m.release_date) as film_cnt_actor
 from {{ ref('unnest_actors')}} u
 join {{ ref('stg_omdb_tmdb_movies_join')}} m 
--- models/marts/mart_writers.sql
+++ models/marts/mart_writers.sql
@@ -8,9 +8,9 @@ select u.writer
      , m.runtime
      , m.metascore 
      , m.imdb_rating
-     , avg(m.profit)      over (partition by u.writer order by m.release_date rows between 2 preceding and current row)   as threefilm_avg_profit
-     , avg(m.imdb_rating) over (partition by u.writer order by m.release_date rows between 2 preceding and current row)   as threefilm_avg_imdb_rating
-     , avg(m.metascore)   over (partition by u.writer order by m.release_date rows between 2 preceding and current row)   as threeday_avg_metascore
+     , to_number(avg(m.profit)      over (partition by u.writer order by m.release_date rows between 2 preceding and current row))   as threefilm_avg_profit
+     , to_number(avg(m.imdb_rating) over (partition by u.writer order by m.release_date rows between 2 preceding and current row),10,1)  as threefilm_avg_imdb_rating
+     , to_number(avg(m.metascore)   over (partition by u.writer order by m.release_date rows between 2 preceding and current row),10,1)   as threeday_avg_metascore
      , row_number()       over (partition by u.writer order by m.release_date) as film_cnt_writer
 from {{ ref('unnest_writers')}} u
 join {{ ref('stg_omdb_tmdb_movies_join')}} m 
