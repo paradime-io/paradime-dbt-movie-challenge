60473f0df1f47ea484faf23f30b52bbb1437c3f5
Philip Song <philip.song@veeva.com>
2024-05-21 22:41:04 +0000

    run model

--- models/sources/stg_tmbd2.sql
+++ models/sources/stg_tmbd2.sql
@@ -1,14 +1,24 @@
-select RELEASE_YEAR,
-ROUND(AVG(IMDB_RATING),1) AS AVG_RATING,
-ROUND(AVG(BOX_OFFICE),1) AS AVG_BOX_OFFICE,
-ROUND(AVG(RUNTIME),1) AS AVG_RUN_TIME,
-count(*) as ct
-FROM
-{{ source('PARADIME_MOVIE_CHALLENGE_EXTRA', 'OMDB_INFLATION_ADJUSTED') }}
-WHERE RELEASE_YEAR>1960
-and box_office is not null
-GROUP BY RELEASE_YEAR
-ORDER BY RELEASE_YEAR ASC
+with genre_unnest as (
+select title,
+RUNTIME,
+IMDB_RATING,
+adj_box_office as BOX_OFFICE,
+release_year,
+TRIM(g.value::string) AS genre
+FROM 
+{{ source('PARADIME_MOVIE_CHALLENGE_FINAL', 'OMDB_INFLATION_ADJUSTED') }} a
+,LATERAL FLATTEN(INPUT => SPLIT(a.genre, ',')) g
+)
 
-
-
+select genre,
+release_year,
+avg(RUNTIME) as avg_runtime,
+ROUND(avg(IMDB_RATING),1) AS AVG_RATING,
+ROUND(AVG(BOX_OFFICE),0) AS AVG_BOX_OFFICE
+FROM genre_unnest
+where release_year between 1990 and 2023
+AND BOX_OFFICE IS NOT NULL
+group by genre,
+release_year
+order by genre,
+release_year
