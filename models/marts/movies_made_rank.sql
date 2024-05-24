3d312762938341c0817ca9c6d158cdcf9489c125
Philip Song <philip.song@veeva.com>
2024-05-24 19:18:33 +0000

    fix

--- models/marts/movies_made_rank.sql
+++ models/marts/movies_made_rank.sql
@@ -20,14 +20,16 @@ GENRE
 ),
 
 
-select * from CT_PRODUCED
---  ranked_by_year as (select 
---  CT_PRODUCED.release_year,
---  CT_PRODUCED.GENRE,
---  RANK() OVER (partition by CT_PRODUCED.RELEASE_YEAR ORDER BY CT_PRODUCED.MOVIES_MADE DESC) AS RNK
---  FROM CT_PRODUCED
---  )
 
+ranked_by_year as (select 
+CT_PRODUCED.release_year,
+CT_PRODUCED.GENRE,
+RANK() OVER (partition by CT_PRODUCED.RELEASE_YEAR ORDER BY CT_PRODUCED.MOVIES_MADE DESC) AS RNK
+FROM CT_PRODUCED
+)
+
+
+select * from CT_PRODUCED
 --  select DISTINCT release_year,
 --  genre,
 --  RNK
