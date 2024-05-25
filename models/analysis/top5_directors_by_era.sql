10a9cd71556875c9dc70345f571aef0c954a9c12
Robertas Kudulis <robertas.kudulis@pigu.lt>
2024-05-25 09:38:39 +0000

    analysis - top5 directors

---
+++ models/analysis/top5_directors_by_era.sql
@@ -0,0 +1,26 @@
+with
+    source as (
+        select
+            epoch,
+            replace(value, '"', '') as director,
+            ifnull(revenue_adj, 0) as revenue_adj
+        from {{ ref('combined_omdb_and_tmdb')}} t1,
+            table(flatten(directors))
+    ),
+
+    final as (
+        select
+            epoch,
+            director,
+            sum(revenue_adj) as total_revenue_adj
+        from source
+        group by 1, 2
+    )
+
+select
+    epoch,
+    director,
+    total_revenue_adj
+from final
+qualify
+    row_number() over (partition by epoch order by total_revenue_adj desc) <= 5
