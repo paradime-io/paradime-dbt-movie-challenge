e301fd96262a0d771d95a7983b3fc8389f067fff
Philip Song <philip.song@veeva.com>
2024-05-25 04:21:55 +0000

    remove file

--- models/marts/Production_movies_made.sql
+++
@@ -1,9 +0,0 @@
---  select production_company,
---  MOVIES_MADE,
---  total_revenue,
---  film_ct_score,
---  film_rev_score,
---  film_ct_score*film_rev_score as combo_score
---  FROM {{ source('PRODUCTION_COMPANIES', 'rev_by_production') }}
---  order by MOVIES_MADE desc
---  limit 50
