3c1f51a8bbe8d82676f148b781cadd7271fe149d
Philip Song <philip.song@veeva.com>
2024-05-25 04:48:09 +0000

    updates

--- models/marts/production_score.sql
+++ models/marts/production_score.sql
@@ -5,5 +5,5 @@ film_ct_score,
 film_rev_score,
 film_ct_score*film_rev_score as combo_score
 FROM {{ source('PRODUCTION_COMPANIES_OVERALL', 'production_companies') }}
-order by combo_score asc
+order by film_ct_score asc
 limit 50
