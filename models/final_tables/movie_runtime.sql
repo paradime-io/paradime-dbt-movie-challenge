5da400f9c03a1057a1dd7714410a2ec422e81277
Viktor Likár <viktor.likar@smart24.hu>
2024-05-26 18:55:02 +0000

    added release year to movie_runtime

--- models/final_tables/movie_runtime.sql
+++ models/final_tables/movie_runtime.sql
@@ -2,6 +2,7 @@ select
     imdb_id,
     title,
     release_date,
+    extract(YEAR FROM release_date) as release_year,
     runtime
 from {{ ref('joined_movies') }}
 where genre not like '%Short%'
