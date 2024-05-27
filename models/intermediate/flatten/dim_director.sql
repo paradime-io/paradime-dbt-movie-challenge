64d7d5ccba0be773d37fa427f6f6227d0cb7d933
Blosher Brar <blosher13@gmail.com>
2024-05-27 01:16:50 +0000

    clean up

--- models/intermediate/flatten/dim_director.sql
+++
@@ -1,6 +0,0 @@
-select
-    tmdb_id,
-    trim(my_split.value) as director
-from  {{ ref('clean_combined_movies') }},
-lateral split_to_table(director,',') as my_split
-where director != 'N/A'
--- models/intermediate/flatten/dim_writers.sql
+++
@@ -1,5 +0,0 @@
-select
-    tmdb_id,
-    trim(my_split.value) as writer
-from  {{ ref('clean_combined_movies') }}, lateral split_to_table(writer,',') as my_split
-where writer != 'N/A'
