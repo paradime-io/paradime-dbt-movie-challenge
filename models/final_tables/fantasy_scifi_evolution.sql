36d6dfb4ffe6228ccfac80f83170a4f36e0cbf41
Viktor Likár <viktor.likar@smart24.hu>
2024-05-27 02:23:16 +0000

    fixed dbt error

--- models/final_tables/fantasy_scifi_evolution.sql
+++ models/final_tables/fantasy_scifi_evolution.sql
@@ -1,7 +1,7 @@
 select 
     *,
     extract(YEAR from release_date) as release_year,
-    concat(EXTRACT(YEAR FROM released_date) - EXTRACT(YEAR FROM released_date) % 10,'s') AS decade,
+    concat(EXTRACT(YEAR FROM release_date) - EXTRACT(YEAR FROM release_date) % 10,'s') AS decade,
     
 from
 {{ ref('fantasy_scifi_movies') }}
